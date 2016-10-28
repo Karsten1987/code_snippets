8d76b3f504c8122226a8a680902ca0b0abd61210
Karsten Knese <karsten.knese@googlemail.com>
2016-10-28 13:43:47 -0700

    map implementation in c

--- c/map.c
+++ c/map.c
@@ -1,70 +1,157 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct _element
 {
-  const char* label;
+  const char* element_data;
 } Element;
 
-typedef struct _element_array
+typedef struct _data_array
 {
-  Element* elements;
+  Element* data;
   unsigned int size;
-} ElementArray;
+} DataArray;
+
+typedef struct _index
+{
+  unsigned int index;
+  const char* label;
+} Index;
 
 typedef struct _map
 {
-  ElementArray* idx_to_elements;
+  Index* index_array;
+  DataArray* data_arrays;
   unsigned int size;
 } Map;
 
-Map initialize()
+void append_to_data_array(DataArray* data_array, Element data)
 {
-  Element e1_1 = {.label = "1_1"};
-  Element e1_2 = {.label = "1_2"};
-  Element e2_1 = {.label = "2_1"};
-  Element e3_1 = {.label = "3_1"};
-  Element e3_2 = {.label = "3_2"};
-  Element e3_3 = {.label = "3_3"};
+  ++data_array->size;
+  data_array->data = realloc(data_array->data, data_array->size*sizeof(Element));
+  data_array->data[data_array->size-1] = data;
+}
 
-  ElementArray e1;
-  e1.elements = malloc(2*sizeof(Element));
-  e1.elements[0] = e1_1;
-  e1.elements[1] = e1_2;
-  e1.size = 2;
+void append_to_map(Map* m, const char* label, Element data)
+{
+  // check wether the label exits already
+  for (unsigned int i=0; i<m->size; ++i)
+  {
+    if (strcmp(m->index_array[i].label, label) == 0)
+    {
+      // label exists already
+      //printf("Found existing label %s. Will append new data\n", m->index_array[i].label);
+      // we will append data into DataArray
+      append_to_data_array(&(m->data_arrays[i]), data);
+      return;
+    }
+  }
 
-  ElementArray e2;
-  e2.elements = malloc(1*sizeof(Element));
-  e2.elements[0] = e2_1;
-  e2.size = 1;
+  //printf("Label %s does not exist yet. Will add a new one\n", label);
+  // when we reach this point, the label does not exist yet
+  // reallocate the index array
+  Index idx = {.index = m->size, .label = label};
+  ++m->size;
+  m->index_array = realloc(m->index_array, m->size*sizeof(Index));
+  m->index_array[idx.index] = idx;
 
-  ElementArray e3;
-  e3.elements = malloc(3*sizeof(Element));
-  e3.elements[0] = e3_1;
-  e3.elements[1] = e3_2;
-  e3.elements[2] = e3_3;
-  e3.size = 3;
+  // reallocate the actual data array
+  m->data_arrays = realloc(m->data_arrays, m->size*sizeof(DataArray));
+  append_to_data_array(&m->data_arrays[idx.index], data);
+}
+
+Map initialize_map()
+{
+  Element d1_1 = {.element_data = "1_1"};
+  Element d1_2 = {.element_data = "1_2"};
+  Element d2_1 = {.element_data = "2_1"};
+  Element d3_1 = {.element_data = "3_1"};
+  Element d3_2 = {.element_data = "3_2"};
+  Element d3_3 = {.element_data = "3_3"};
+
+  DataArray d1;
+  d1.data = malloc(2*sizeof(Element));
+  d1.data[0] = d1_1;
+  d1.data[1] = d1_2;
+  d1.size = 2;
+
+  DataArray d2;
+  d2.data = malloc(1*sizeof(Element));
+  d2.data[0] = d2_1;
+  d2.size = 1;
+
+  DataArray d3;
+  d3.data = malloc(3*sizeof(Element));
+  d3.data[0] = d3_1;
+  d3.data[1] = d3_2;
+  d3.data[2] = d3_3;
+  d3.size = 3;
+
+  Index idx1 = {.index = 0, .label = "Index 1"};
+  Index idx2 = {.index = 1, .label = "Index 2"};
+  Index idx3 = {.index = 2, .label = "Index 3"};
 
   Map m;
-  m.idx_to_elements = malloc(3*sizeof(ElementArray));
-  m.idx_to_elements[0] = e1;
-  m.idx_to_elements[1] = e2;
-  m.idx_to_elements[2] = e3;
+  m.index_array = malloc(3*sizeof(Index));
+  m.index_array[idx1.index] = idx1;
+  m.index_array[idx2.index] = idx2;
+  m.index_array[idx3.index] = idx3;
+
+  m.data_arrays = malloc(3*sizeof(DataArray));
+  m.data_arrays[0] = d1;
+  m.data_arrays[1] = d2;
+  m.data_arrays[2] = d3;
   m.size = 3;
+
   return m;
+}
+
+DataArray* get_map_index(Map* m, const char* label)
+{
+  for (unsigned int i=0; i<m->size; ++i)
+  {
+    if (strcmp(m->index_array[i].label, label) == 0)
+    {
+      return &m->data_arrays[i];
+    }
+  }
+  return NULL;
+}
+
+void print_data_array(const DataArray* da)
+{
+    for (unsigned int i=0; i<da->size; ++i)
+    {
+      printf("%s\t", da->data[i].element_data);
+    }
+    printf("\n");
+}
+
+void print_map(const Map* m)
+{
+  printf("Map size %u\n", m->size);
+  for (unsigned int i=0; i<m->size; ++i)
+  {
+    printf("label %s ::: ", m->index_array[i].label);
+    print_data_array(&(m->data_arrays[i]));
+  }
 }
 
 int main()
 {
-  Map m = initialize();
+  Map m = initialize_map();
+  print_map(&m);
 
-  for (unsigned int i=0; i<m.size; ++i)
-  {
-    for (unsigned int j=0; j<m.idx_to_elements[i].size; ++j)
-    {
-      printf("%s\t", m.idx_to_elements[i].elements[j].label);
-    }
-    printf("\n");
-  }
+  Element d_new = {.element_data = "1_3"};
+  append_to_map(&m, "Index 1", d_new);
+  print_map(&m);
+
+  Element d_new_new = {.element_data = "4_1"};
+  append_to_map(&m, "Index 4", d_new_new);
+  print_map(&m);
+
+  DataArray* index_1 = get_map_index(&m, "Index 1");
+  print_data_array(index_1);
   return 0;
 }
