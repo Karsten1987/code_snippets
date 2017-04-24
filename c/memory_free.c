761c2f2b64e72c112bd0e18edce827b29943e539
Karsten Knese <karsten.knese@googlemail.com>
2017-04-23 17:01:44 -0700

    more c stuff

--- c/memory_free.c
+++ c/memory_free.c
@@ -2,24 +2,57 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+typedef struct my_struct_
+{
+  size_t size;
+  char ** data;
+} MyStruct;
+
+size_t my_struct_fini(MyStruct * ms)
+{
+  if (!ms){
+    return 1;
+  }
+
+  for (size_t i = 0; i < ms->size; ++i) {
+    if (ms->data[i]) {
+      free(ms->data[i]);
+      ms->data[i] = NULL;
+    }
+  }
+
+  if (ms->data) {
+    free(ms->data);
+    ms->data = NULL;
+  }
+
+  return 0;
+}
+
 int main(int argc, char** argv)
 {
-  unsigned long array_size = 100000000000000;
-  unsigned long memory_size = (array_size*sizeof(int))/1024;
-  printf("expected memory > %lu\n", memory_size);
-  int* data_array = calloc(array_size, sizeof(int));
 
-  int counter = 0;
-  while(counter != 100)
-  {
-    long busy_loop = 0;
-    while (busy_loop < 1000)
-    {
-      busy_loop++;
-    }
-    counter++;
-    sleep(1);  // one second
-  }
-  free(data_array);
+  // doesn't work because size_t might be shitty
+  MyStruct ms;
+  //ms.size = 0;
+  my_struct_fini(&ms);
+
+  //unsigned long array_size = 100000000000000;
+  //unsigned long memory_size = (array_size*sizeof(int))/1024;
+  //printf("expected memory > %lu\n", memory_size);
+  //int* data_array = calloc(array_size, sizeof(int));
+
+  //int counter = 0;
+  //while(counter != 100)
+  //{
+  //  long busy_loop = 0;
+  //  while (busy_loop < 1000)
+  //  {
+  //    busy_loop++;
+  //  }
+  //  counter++;
+  //  sleep(1);  // one second
+  //}
+  //free(data_array);
   return 0;
 }
--- cpp/anagram.cpp
+++ cpp/anagram.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <algorithm>
+#include <unordered_map>
 
-bool isAnagram( std::string s1, std::string s2 )
+bool isAnagram_sort( std::string s1, std::string s2 )
 {
   if (s1.size() != s2.size())
   {
@@ -21,8 +22,45 @@ bool isAnagram( std::string s1, std::string s2 )
   return true;
 }
 
+bool isAnagram_map( std::string s1, std::string s2 )
+{
+  if (s1.size() != s2.size()) {
+    return false;
+  }
+
+  std::unordered_map<char, size_t> map_s1;
+
+  for (auto i = 0; i < s1.size(); ++i)
+  {
+    if (map_s1.count(s1[i]) > 0) {
+      ++map_s1[s1[i]];
+    } else {
+      map_s1.insert(std::pair<char, int>(s1[i], 1));
+    }
+  }
+
+  for (auto i = 0; i < s2.size(); ++i)
+  {
+    if (map_s1.count(s2[i]) == 0) {
+      return false;
+    } else {
+      --map_s1[s2[i]];
+    }
+  }
+
+  for (auto it = map_s1.begin(); it != map_s1.end(); ++it)
+  {
+    if (it->second != 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
 int main()
 {
-  std::cerr << " is anagram " << isAnagram( "abcd", "dcba" ) << std::endl;
+  std::string base_string = "abcd";
+  std::string test_string = "ddbd";
+  std::cerr << base_string << " and " << test_string << " are anagrams: " << std::boolalpha << isAnagram_map( base_string, test_string ) << std::endl;
   return 0;
 }
